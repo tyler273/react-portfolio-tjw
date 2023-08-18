3cba586ab0b1bfa713a72f97362ed331127dd7bf
Tyler <t.woods95@aol.com>
2023-08-18 18:16:04 -0400

    added critter tracker image and project file, updated portfolio file

---
+++ src/components/Project.jsx
@@ -0,0 +1,6 @@
+export default function Project() {
+    return (
+        <div>hi</div>
+    )
+
+}
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,15 +1,36 @@
+import Project from "../components/Project";
+import critterTracker from "../assets/critterTracker.png";
+
+
 export default function Portfolio() {
-    return (
-      <div>
-        <h1>Portfolio Page</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
-      </div>
-    );
-  }
+  const projs = [
+    {
+      name: "Critter Tracker",
+      deployedLink: "https://critter-tracker-p2-d4b8f730eb59.herokuapp.com/",
+      repoLink: "https://github.com/tyler273/critter-tracker",
+      image: critterTracker
+    }
+  ]
+
+  return (
+    <section>
+      <h2>Projects</h2>
+      <ul>
+      {
+        projs.map((project, i) => (
+          <li key = {i}>
+            {project.name}
+            <img src={project.image} alt={project.name}/>
+            <a href={project.deployedLink}>
+              Deployed Link
+            </a>
+            <a href={project.repoLink}>
+              GitHub Repo
+            </a>
+          </li>
+        ))
+      }
+      </ul>
+    </section>
+  )
+};
