17b5f17162b349d8c714f66c3f6c5010fa2b4155
Tyler <t.woods95@aol.com>
2023-08-24 22:37:31 -0400

    added profile pic and about me text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,13 @@
+import React from "react";
+import profilepic from "../assets/selfie.jpeg";
+
 export default function About() {
     return (
       <div>
-        <h1>About Page</h1>
+        <h1>About Me</h1>
+        <img className="profile-pic" src={profilepic} alt="a photo of me" />
         <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        My name is Tyler Woods and I am new to the field of full-stack development! I was previously in health care and decided on a career change which led me to enroll in UPenn's coding bootcamp. I was born and raised in Philadelphia where I attended CHS and graduated with an associate's degree from the Community College of Philadelphia. This website will be updated as I learn more and improve my skills in both HTML, CSS and JavaScript!
         </p>
       </div>
     );
