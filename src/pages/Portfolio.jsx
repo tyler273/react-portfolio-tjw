6e1fc7d6e30ed9d6012bd322bc64006279d7d3a7
Tyler <t.woods95@aol.com>
2023-09-05 04:01:26 -0400

    added more projects to portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,11 +1,20 @@
 import Project from "../components/Project";
+import movieVault from "../assets/movieVault.png";
 import critterTracker from "../assets/critterTracker.png";
 import foodFocus from "../assets/foodFocus.png";
+import techTalk from "../assets/techTalk.png";
 import pwGenerator from "../assets/pwGenerator.png";
+import noteTaker from "../assets/noteTaker.png";
 
 
 export default function Portfolio() {
   const projs = [
+    {
+      name: "Movie Vault",
+      deployedLink: "https://critter-tracker-p2-d4b8f730eb59.herokuapp.com/",
+      repoLink: "https://github.com/nolannaphys/movie-vault",
+      image: movieVault
+    },
     {
       name: "Critter Tracker",
       deployedLink: "https://critter-tracker-p2-d4b8f730eb59.herokuapp.com/",
@@ -19,10 +28,22 @@ export default function Portfolio() {
       image: foodFocus
     },
     {
+      name: "Tech Talk Blog",
+      deployedLink: "https://tech-talk-blog-tjw-3a20aff9c737.herokuapp.com/",
+      repoLink: "https://github.com/tyler273/tech-talk-blog",
+      image: techTalk
+    },
+    {
       name: "Password Generator",
       deployedLink: "https://tyler273.github.io/password-generator/",
       repoLink: "https://github.com/tyler273/password-generator",
       image: pwGenerator
+    },
+    {
+      name: "Note Taker",
+      deployedLink: "https://note-taker-tjw.herokuapp.com/",
+      repoLink: "https://github.com/tyler273/note-taker",
+      image: noteTaker
     },
   ]
 
