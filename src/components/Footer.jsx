b1a7c220de32a951693a11240a0ec31d313e58ad
Tyler <t.woods95@aol.com>
2023-09-05 02:46:56 -0400

    Fixed logos and their size in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import "../index.css";
 // import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 
 function Footer(){
     return(
         <footer>
             <a href="https://github.com/tyler273">
-            <FontAwesomeIcon icon={faGithub} style={{color: "#668cff"}} />
+                <FontAwesomeIcon icon={faGithub} className="fa-3x" style={{color: "#668cff"}} />
             </a>
             <a href="https://www.linkedin.com/in/tyler-woods273/">
-            <FontAwesomeIcon icon={faLinkedin} style={{color: "#668cff"}} />
+                <FontAwesomeIcon icon={faLinkedin} className="fa-3x" style={{color: "#668cff"}} />
+            </a>
+            <a href="https://stackexchange.com/users/29368411/tyler273">
+                <FontAwesomeIcon icon={faStackOverflow} className="fa-3x" style={{color: "#668cff"}} />
             </a>
         </footer>
     );
