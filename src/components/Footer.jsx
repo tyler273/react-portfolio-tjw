c425d72599e5397f4a55d5647f456a5e13663279
Tyler <t.woods95@aol.com>
2023-09-05 01:01:09 -0400

    Added footer code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../index.css";
+// import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-solid-svg-icons';
+
+function Footer(){
+    return(
+        <footer>
+            <a href="https://github.com/tyler273">
+            <FontAwesomeIcon icon={faGithub} style={{color: "#668cff"}} />
+            </a>
+            <a href="">
+            <FontAwesomeIcon icon={faLinkedin} style={{color: "#668cff"}} />
+            </a>
+        </footer>
+    );
+}
+
+export default Footer;
