60d88038fca946a00a424217b4f7ea3c0e6bd8d2
Tyler <t.woods95@aol.com>
2023-09-12 08:12:09 -0400

    Updated about me text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,13 +11,13 @@ export default function About() {
         </section>
         <section className="text-container">
           <p>
-            Hello there! My name is Tyler Woods and I am new to the field of full-stack development!
+            Hello there! My name is Tyler Woods and I am a Full-Stack Web Developer!
           </p>
           <p>
             I was born and raised in Philadelphia where I attended Central High School and graduated with an associate's degree from the Community College of Philadelphia. I was previously in health care and decided on a career change which led me to enroll in the University of Pennsylvania's coding bootcamp where I earned my certificate of completion for full-stack web development!
           </p>
           <p>
-            Having completing the University of Pennsylvania's full-stack bootcamp, I can confidently say that I love coding and am very eager to get into the field. Teamwork and collaboration is my strong suit and I look forward to building projects with other amazing people in tech!
+            Having completing the University of Pennsylvania's full-stack bootcamp, I can confidently say that I love coding and am very eager to get into the field. Teamwork, collaboration and the desire to learn more skills are my main strengths and I look forward to building projects with other amazing people in tech!
           </p>
         </section>
       </div>
