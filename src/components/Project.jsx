d7e7565cb9eff23f8011a7857dbe58342f8c9c2f
Tyler <t.woods95@aol.com>
2023-09-17 01:50:07 -0400

    Added card flip still WiP for projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+
 export default function Project({project}) {
-    return (
-        <div className="card">
-        <img className="card-img-top" src={project.image} alt={project.name}/>
-        <div className="card-detail">
-        {project.name}
-        <a href={project.deployedLink}>
-          Deployed Link
-        </a>
-        <a href={project.repoLink}>
-          GitHub Repo
-        </a>
-        </div>
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  function flipCard(){
+    setIsFlipped(!isFlipped);
+  }
+
+  return (
+      <div className="card">
+        <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped}>
+          <div className="card-front" onClick={flipCard}>
+            <img className="card-img-top" src={project.image} alt={project.name}/>
+          </div>
+          <div className="card-detail card-back" onClick={flipCard}>
+            {project.name}
+            <a href={project.deployedLink}>
+              Deployed Link
+            </a>
+            <a href={project.repoLink}>
+              GitHub Repo
+            </a>
+          </div>
+        </ReactCardFlip>
       </div>
-    )
+  )
 
 }
