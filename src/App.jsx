6187387c2820c442e6dba162e089df773c75cb19
Tyler <t.woods95@aol.com>
2023-09-19 00:24:30 -0400

    made footer fixed to bottom with flexbox and added app css

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Background from './components/Background';
 import Footer from './components/Footer';
+import "./styles/App.css";
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
-    <>
+    <div className="App">
       <Header />
       <Background />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
 
