314447b565fd3f397ae4cb2bbd9b130dfc563106
Tyler <t.woods95@aol.com>
2023-09-19 18:49:11 -0400

    added email address visibly

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -118,9 +118,10 @@ function Contact() {
   }
 
   return (
-    <div className="contact-container">
-      <h4>Fill in this contact form and I will get back to you as soon as possible!</h4>
-      <Form action="https://formspree.io/f/xbjvkavk" method="POST">
+    <div className="contact-wrapper">
+      <section className="contact-container">
+        <h4>Fill in this contact form and I will get back to you as soon as possible!</h4>
+        <Form action="https://formspree.io/f/xbjvkavk" method="POST">
         <Form.Group className="col-5 mb-3" controlId="formBasicText">
           <Form.Label>Name</Form.Label>
           <Form.Control 
@@ -162,6 +163,13 @@ function Contact() {
             Submit
           </Button>
       </Form>
+        <section className="contact-info">
+          <ul>
+            <li>Email: tylerwoods468@gmail.com</li>
+            <li>Phone: 267-886-6651</li>
+          </ul>
+        </section>
+      </section>
     </div>  
   );
 }
